5e1aa9c94892aa6080afd716a994d50fd904a9d2
cioddi <maxtobiasweber@gmail.com>
2013-08-24 19:01:02 +0200

    initial commit

--- tests/setup/before_script.sh
+++ tests/setup/before_script.sh
@@ -1,4 +1,5 @@
+curl -sS https://getcomposer.org/installer | php
 php composer.phar require "phpunit/phpunit=3.7.*"
 
-pear channel-discover pear.phpunit.de
-pear install phpunit/PHPUnit
+# pear channel-discover pear.phpunit.de
+# pear install phpunit/PHPUnit
